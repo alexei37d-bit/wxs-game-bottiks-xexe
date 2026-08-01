1858ad2ee6317d6b082c6ce925695ae5b7bef99e
alexei37d-bit <alexei37d@gmail.com>
2026-08-02 00:29:22 +0700

    Delete handlers directory

--- handlers/user_states.py
+++
@@ -1,5 +0,0 @@
-from aiogram.fsm.state import StatesGroup, State
-
-class CubeGame(StatesGroup):
-    bet = State()      # Ввод суммы ставки
-    number = State()   # Выбор точного числа (если выбран этот режим)
